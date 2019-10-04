bc923e1cb595d656353965cd9bc36833f86f576f
brunogeek9 <brunogeek9@gmail.com>
2019-10-03 23:05:57 -0300

    iniciando o projeto

---
+++ tests/TestCase/Controller/ArticlesControllerTest.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Test\TestCase\Controller;
+
+use App\Controller\ArticlesController;
+use Cake\TestSuite\IntegrationTestTrait;
+use Cake\TestSuite\TestCase;
+
+/**
+ * App\Controller\ArticlesController Test Case
+ *
+ * @uses \App\Controller\ArticlesController
+ */
+class ArticlesControllerTest extends TestCase
+{
+    use IntegrationTestTrait;
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'app.Articles'
+    ];
+
+    /**
+     * Test index method
+     *
+     * @return void
+     */
+    public function testIndex()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
